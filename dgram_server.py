59df13185b1b93460405e0fbf663e688dfcb1065
Divine Darkey <teddbug47@outlook.com>
2020-11-19 15:57:44 +0000

    Update dgram_server.py

--- dgram_server.py
+++ dgram_server.py
@@ -8,7 +8,7 @@ MSG = b"Hey client, we have received your data."
 with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as dgram:
     # binding the socket to an ip and a port but in this case
     # I'll use empty quotes to allow any ip to connect
-    dgram.bind(("", 2525))
+    dgram.bind((socket.gethostname(), 2525))
     # receiving data from any client who tries to connect
     msg, (address, port) = dgram.recvfrom(DATA_SIZE)
     print(f"[+]: Connection established from {address} on port {port}.")
